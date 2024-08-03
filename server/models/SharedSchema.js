bf589bbda4f24318150b69ad7e16572ee94a97bd
Jason Stiltner <jasonstiltner@gmail.com>
2024-08-02 20:23:03 -0500

    created swapWith method on prototype model

--- server/models/SharedSchema.js
+++ server/models/SharedSchema.js
@@ -2,17 +2,50 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const sharedSchema = new Schema({
-  parentId: Schema.Types.ObjectId,
-  children: [Schema.Types.ObjectId,],
-  updated: { type: Date, default: Date.now}
+  parent: { type: Schema.Types.ObjectId },
+  children: [{ type: Schema.Types.ObjectId }],
+  updated: { type: Date, default: Date.now }
 });
 
-sharedSchema.methods.swapWith = function(swapMate) {
-  // swapWith peer-level object in same or different parent class object 
-  // update parent fields on child objects
-  // update child arrays on parent objects
-}; 
+sharedSchema.methods.swapWith = async function(swapMateId) {  
+  
+  // Method to swap this another peer
+    // update child arrays on parent objects
+    try {
+      const swapMate = await mongoose.Model.findById(swapMateId);
+      if (!swapMate) throw new Error('swapMateId not found');
 
+    const currentParent = this.parent;
+    const swapParent = swapMate.parent;
+
+    // Update the parent of the current thing
+    this.parent = swapParent;
+    await this.save();
+
+    // Update the parent of the swapMate thing
+    swapMate.parent = currentParent;
+    await swapMate.save();
+
+    // Update children arrays of the respective parents
+    if (currentParent) {
+      await mongoose.Model.findByIdAndUpdate(currentParent, {
+        $pull: { children: this._id },
+        $addToSet: { children: swapMate._id }
+      });
+    }
+
+    if (swapParent) {
+      await mongoose.Model.findByIdAndUpdate(swapParent, {
+        $pull: { children: swapMate._id },
+        $addToSet: { children: this._id }
+      });
+    }
+
+    return { success: true, message: "Swap successful" };
+  } catch (error) {
+    return { success: false, message: error.message };
+  }
+};
   
 // moveTo different parent object
 sharedSchema.methods.moveTo = function(targetParent) { 
